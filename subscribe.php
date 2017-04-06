8a2363cea0c9e3d00047f5c9cb4db0cc8cdab0f3
Nathan Mills <smills1149@gmail.com>
2017-04-06 17:41:32 -0500

    pleasework

---
+++ subscribe.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['name'];
+?>
+
+<?php
+$email_from = 'smills1149@gmail.com';
+$email_subject = "new person";
+$email_body = "here's the new person: $name.\n".
+?>
+
+<?php
+$to = "smills1149@gmail.com";
+$headers = "From: $email_from \r\n";
+mail($to,$email_subject,$email_body,$headers);
+?>
